f2b9e767a94c5432a4f1aa9964a8e618a65b30eb
jonathan450 <44813750+jonathan450@users.noreply.github.com>
2018-11-20 10:26:40 -0700

    Add files via upload

---
+++ even_num03.py
@@ -0,0 +1,18 @@
+"Using input() function "
+
+a = int(input("Enter the value :"))
+
+print(a)
+
+b = int(input("Enter a second number:"))
+
+print(b)
+
+print(a+b)
+
+num = int(input("Enter the value :"))
+
+if num%2 == 0:
+    print('Number :',num, "is Even")
+else:
+    print('Number :',num, "is Odd")
